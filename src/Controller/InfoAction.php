f28fc5974f97c1e678360f0ebd43175f002106da
Sergii Shymko <sergey@shymko.net>
2016-09-15 22:09:40 -0700

    Implement dependency injection in sample action
    - Inject request service into the sample action class
    - Include server info into the same action response

--- src/Controller/InfoAction.php
+++ src/Controller/InfoAction.php
@@ -2,16 +2,34 @@
 namespace Upscale\HttpServerSkeleton\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Upscale\HttpServerEngine\ActionInterface;
+use Zend\Diactoros\ServerRequestFactory;
 
 class InfoAction implements ActionInterface
 {
+    /**
+     * @var ServerRequestInterface
+     */
+    protected $request;
+
+    /**
+     * Inject dependencies
+     *
+     * @param ServerRequestInterface $request
+     */
+    public function __construct(ServerRequestInterface $request)
+    {
+        $this->request = $request;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function execute(ResponseInterface $response)
     {
-        $response->getBody()->write('Server is running!');
+        $software = ServerRequestFactory::get('SERVER_SOFTWARE', $this->request->getServerParams(), 'PHP');
+        $response->getBody()->write("Server powered by $software is running!");
         return $response;
     }
 }
